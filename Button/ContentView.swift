0ad576d699caa45bd43e1d44f02b8500c50fafb1
Will Townsend <will@townsend.io>
2019-12-06 19:36:21 -0800

    Add initial CustomButton View
    This uses the gestures to track when you pan away from the button to un-press it.

--- Button/ContentView.swift
+++ Button/ContentView.swift
@@ -8,10 +8,73 @@
 
 import SwiftUI
 
+struct CustomButton: View {
+
+    private enum ButtonGestureState {
+        case inactive
+        case pressing
+        case outOfBounds
+
+        var isPressed: Bool {
+            switch self {
+            case .pressing:
+                return true
+            default:
+                return false
+            }
+        }
+    }
+
+    @GestureState private var dragState = ButtonGestureState.inactive
+    @State private var isPressed = false
+
+    let action: () -> Void
+
+    var body: some View {
+
+        let dragGesture = DragGesture(minimumDistance: 0)
+            .updating($dragState, body: { value, state, transaction in
+                let distance = sqrt(
+                    abs(value.translation.height) + abs(value.translation.width)
+                )
+
+                if distance > 10 {
+                    state = .outOfBounds
+                } else {
+                    state = .pressing
+                }
+            })
+            .onChanged({ value in
+                withAnimation(.easeInOut(duration: 0.16)) {
+                    self.isPressed = self.dragState.isPressed
+                }
+            })
+            .onEnded { _ in
+                if self.isPressed {
+                    self.action()
+                }
+
+                withAnimation(.easeInOut(duration: 0.16)) {
+                    self.isPressed = self.dragState.isPressed
+                }
+            }
+
+        return Text("Hello, World!")
+                .foregroundColor(.blue)
+                .opacity(isPressed ? 0.3 : 1.0)
+                .gesture(dragGesture)
+
+    }
+}
+
 struct ContentView: View {
     var body: some View {
 
         VStack {
+            CustomButton() {
+                dump("tappped")
+                return
+            }
             Button("This is a long button") {
                 dump("tapped")
                 return
