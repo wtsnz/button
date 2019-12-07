89189637f4c7638c0c1a48157825672167fe6c87
Will Townsend <will@townsend.io>
2019-12-06 19:35:09 -0800

    Initial commit

---
+++ Button/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  Button
+//
+//  Created by Will Townsend on 2019-12-06.
+//  Copyright © 2019 Overflight. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+
+        VStack {
+            Button("This is a long button") {
+                dump("tapped")
+                return
+            }
+        }
+
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
